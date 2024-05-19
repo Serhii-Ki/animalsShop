0772f1e16d5c93bb36fede9ad09933f904fbf467
Kiba Sergey <serhiikiba@gmail.com>
2024-05-19 19:36:14 +0200

    started to do slider

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 import './style.scss';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})

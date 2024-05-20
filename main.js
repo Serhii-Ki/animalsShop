a178571015f3a9adecd1dbba5633ba00be7f2d9f
Kiba Sergey <serhiikiba@gmail.com>
2024-05-20 20:04:59 +0200

    finished proj

--- main.js
+++ main.js
@@ -5,20 +5,49 @@ import 'swiper/css/bundle';
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
+  parallax: true,
+  speed: 1000,
+  keyboard: {
+    enabled: true
+  },
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.slider-controls__count',
+    type: "fraction"
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '#sliderNext',
+    prevEl: '#sliderPrev',
   },
 
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
   },
-})
+});
+
+
+const overlay = document.querySelector('.mobile-overlay');
+const mobileNav = document.querySelector('.mobile-nav--content');
+
+function toggleMobileNav() {
+  overlay.classList.toggle('mobile-overlay--open');
+  mobileNav.classList.toggle('mobile-nav--content--open');
+}
+
+document.querySelector('.nav-btn').addEventListener('click', () => {
+  toggleMobileNav()
+});
+
+document.querySelector('.nav-btn--content').addEventListener('click', () => {
+  toggleMobileNav()
+});
+
+overlay.addEventListener('click', (e) => {
+  if (e.target.classList.contains('mobile-overlay')) {
+    toggleMobileNav()
+  }
+});
